172308d29058ae9a4a000ae3dafec5af718ac2a3
TheCinemaker <avar.szilveszter@gmail.com>
2026-08-06 12:03:54 +0200

    Update reviews database with 6 complete product reviews including Teendow C7 Max

---
+++ frontend/build/precache-manifest.09310ba2fe8bd96466b4c311d804c044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00a2d0f5aa9c6f5ed96c81298696edf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3dc2a188af42fcfab05",
+    "url": "/static/css/main.f1229817.chunk.css"
+  },
+  {
+    "revision": "136501d507085d851d27",
+    "url": "/static/js/2.36d783ff.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.36d783ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a017023aba11dfbcdc",
+    "url": "/static/js/3.48f0057e.chunk.js"
+  },
+  {
+    "revision": "f3dc2a188af42fcfab05",
+    "url": "/static/js/main.6ea870fe.chunk.js"
+  },
+  {
+    "revision": "b1cddba738ad0cfcb93f",
+    "url": "/static/js/runtime-main.3b79c33e.js"
+  }
+]);
